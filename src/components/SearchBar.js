bf1bd9e56a748bd6a77206997411a93d575ad697
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-13 15:53:29 -0700

    added search bar icons and styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
-    <View>
+    <View style={styles.background}>
+      <EvilIcons name="search" size={30} />
       <Text>Search Bar</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15
+  }
+});
 
 
 export default SearchBar;
