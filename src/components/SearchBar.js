1b1c703504ae4a60773747f14fa5c791510e38c7
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-13 16:10:24 -0700

    added state and incorporated it within searchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { EvilIcons } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.backgroundStyle}>
       <EvilIcons
       name="search" 
-      size={30}
+      style={styles.iconStyle} 
       />
 
       <TextInput
       style={styles.inputStyle} 
       placeholder='Search'
+      value={term}
+      onChangeText={newTerm => onTermChange(newTerm)}
+      autoCapitalize="none"
+      autoCorrect={false}
       />
     </View>
   );
@@ -24,12 +28,16 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
-    flexDirection: 'row'
+    flexDirection: 'row',
+    marginTop: 10
   },
   inputStyle: {
-    borderColor: 'black',
-    borderWidth: 1,
+    fontSize: 18,
     flex: 1
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center'
   }
 });
 
