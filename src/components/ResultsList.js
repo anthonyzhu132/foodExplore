9d8f98504d3f635bf161a6542d107b41460c8adc
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-14 14:59:01 -0700

    started ResultsList.js component

---
+++ src/components/ResultsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultsList = ({ title }) => {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  );
+}
+
+export default ResultsList;
