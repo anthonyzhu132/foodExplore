ef906e884ef35c69e3941b3e721b4b14f30b9d86
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-14 14:53:03 -0700

    refactored and moved business logic into hooks folder

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,19 @@
-import React, {useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [error, setError] = useState('');
-
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term,
-          location: 'Vancouver'
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setError('Error')
-    }
-  }
+  const [searchApi, results, error] = useResults();
 
   return (
     <View>
       <SearchBar 
       term={term} 
       onTermChange={(newTerm) => setTerm(newTerm) }
-      ontermSubmit={() => searchApi()}
+      ontermSubmit={() => searchApi(term)}
       />
       {error ? <Text>{error}</Text> : null}
       <Text>Found {results.length} results </Text>
