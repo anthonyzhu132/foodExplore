a237ce304124b98e726bc7b1c37c1a43c6770206
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-14 15:01:48 -0700

    added basic styling to titles for each group

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,9 +4,16 @@ import { View, Text, StyleSheet } from 'react-native';
 const ResultsList = ({ title }) => {
   return (
     <View>
-      <Text>{title}</Text>
+      <Text style={styles.titleStyle}>{title}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
 
 export default ResultsList;
